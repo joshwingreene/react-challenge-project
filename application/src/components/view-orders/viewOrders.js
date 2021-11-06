32b45a894c920999a7c541c9f89a6eee7ab9ae48
Joshwin Greene <joshwin.greene@gmail.com>
2021-11-06 16:34:52 -0700

    feat(viewOrders): adds delete functionality

--- application/src/components/view-orders/viewOrders.js
+++ application/src/components/view-orders/viewOrders.js
@@ -3,21 +3,52 @@ import { Template } from '../../components';
 import { SERVER_IP } from '../../private';
 import './viewOrders.css';
 
+const DELETE_ORDER_URL = `${SERVER_IP}/api/delete-order`;
+
 class ViewOrders extends Component {
     state = {
-        orders: []
+        orders: [],
+        isLoading: true
     }
 
-    componentDidMount() {
+    fetchCurrentOrders() {
         fetch(`${SERVER_IP}/api/current-orders`)
             .then(response => response.json())
             .then(response => {
                 if(response.success) {
-                    this.setState({ orders: response.orders });
+                    this.setState({ orders: response.orders, isLoading: false });
                 } else {
                     console.log('Error getting orders');
                 }
-            });
+        });
+    }
+
+    componentDidMount() {
+        this.fetchCurrentOrders();
+    }
+
+    componentDidUpdate() {
+        if (this.state.isLoading) {
+            this.fetchCurrentOrders();
+        }
+    }
+
+    deleteOrder(order_id) {
+        fetch(DELETE_ORDER_URL, {
+            method: 'POST',
+            body: JSON.stringify({
+                id: order_id
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(res => res.json())
+        .then(response => { 
+            console.log("Success", JSON.stringify(response));
+            this.setState({ isLoading: true });
+        })
+        .catch(error => console.error(error));
     }
 
     render() {
@@ -38,7 +69,7 @@ class ViewOrders extends Component {
                                  </div>
                                  <div className="col-md-4 view-order-right-col">
                                      <button className="btn btn-success">Edit</button>
-                                     <button className="btn btn-danger">Delete</button>
+                                     <button className="btn btn-danger" onClick={() => this.deleteOrder(order._id)}>Delete</button>
                                  </div>
                             </div>
                         );
