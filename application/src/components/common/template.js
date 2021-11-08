59d4a6d904753815df57859c7183f00c08571de2
Joshwin Greene <joshwin.greene@gmail.com>
2021-11-07 16:34:57 -0800

    feat(logout): adds logout functionality

--- application/src/components/common/template.js
+++ application/src/components/common/template.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Nav } from '../../components';
+import { logoutUser } from '../../redux/actions/authActions';
 import './template.css';
+
+const mapActionsToProps = dispatch => ({
+    commenceLogout() {
+      dispatch(logoutUser());
+    }
+})
 
 const Template = props => {
     return (
         <div className="bg-layer">
             <div className="fg-layer">
                 <label className="logo">Bruce's Diner</label>
-                <Nav />
+                <Nav commenceLogout={props.commenceLogout} />
                 {props.children}
             </div>
         </div>
     );
 }
 
-export default Template;
+export default connect(null, mapActionsToProps)(Template);
